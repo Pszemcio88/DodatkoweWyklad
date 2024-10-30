84c0f9afba39070efc9635608023e7a2d2ec3311
Pszemo888 <przemus.unknown@gmai.com>
2024-10-30 22:28:52 +0100

    komponenty

---
+++ src/app/business-card/business-card.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ShowDateComponent } from '../show-date/show-date.component';
+import { CommonModule } from '@angular/common';
+import { ManageInterestsComponent } from '../manage-interests/manage-interests.component';
+
+@Component({
+  selector: 'app-business-card',
+  standalone: true,
+  imports: [FormsModule,ShowDateComponent,CommonModule, ManageInterestsComponent],
+  templateUrl: './business-card.component.html',
+  styleUrl: './business-card.component.css'
+})
+export class BusinessCardComponent {
+  name: string;
+  surname: string;
+  dateOfBirth: Date;
+  interests: string[];
+  selected:number=-1;
+
+  students: string[] = [
+    'Zbyszko ',
+    'Krzychu '
+  ];
+  studentCount: { [key: string]: string } = {
+    '=0': 'brak studentów',
+    '=1': 'student',
+    'other': 'studentów'
+  };
+  constructor() { 
+    this.name = 'Joanna';
+    this.surname = 'Krupa';
+    this.dateOfBirth = new Date('2000-03-18');
+    this.interests = ['tennis', 'programming', 'photography'];
+  }
+
+  selectInterest(which:number):void{
+      this.selected=which;
+  }
+
+  
+}
